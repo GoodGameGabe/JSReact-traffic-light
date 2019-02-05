193adc1baca2c1c794dae536806aecebb73e65fb
Gabriel Herndon <gherndon5@gmail.com>
2019-02-05 17:26:09 +0000

    initial build

--- react-hello/src/js/component/home.jsx
+++ react-hello/src/js/component/home.jsx
@@ -8,7 +8,7 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
+				<h1>Hello Rigoeeeeee!</h1>
 				<p>
 					<img src={rigoImage} />
 				</p>
